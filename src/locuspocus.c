93bb3f1e91c518ad37266c64b36cc7f550be0b79
Daniel Standage <daniel.standage@gmail.com>
2014-01-22 21:52:18 -0500

    Replaced --emptyseqs with --skipends

--- src/locuspocus.c
+++ src/locuspocus.c
@@ -12,7 +12,6 @@ typedef struct
   unsigned long delta;
   FILE *outstream;
   bool skipends;
-  bool reportemptyseqs;
   FILE *transstream;
   bool verbose;
 } LocusPocusOptions;
@@ -23,11 +22,10 @@ void parse_options(int argc, char **argv, LocusPocusOptions *options)
 {
   int opt = 0;
   int optindex = 0;
-  const char *optstr = "deg:hil:n:o:st:v";
+  const char *optstr = "dg:hil:n:o:st:v";
   const struct option locuspocus_options[] =
   {
     { "debug",     no_argument,       NULL, 'd' },
-    { "emptyseqs", no_argument,       NULL, 'e' },
     { "genemap",   required_argument, NULL, 'g' },
     { "help",      no_argument,       NULL, 'h' },
     { "intloci",   no_argument,       NULL, 'i' },
@@ -46,9 +44,6 @@ void parse_options(int argc, char **argv, LocusPocusOptions *options)
       case 'd':
         options->debug = 1;
         options->verbose = 1;
-        break;
-      case 'e':
-        options->reportemptyseqs = 1;
         break;
       case 'g':
         options->genestream = fopen(optarg, "w");
@@ -110,8 +105,6 @@ void print_usage(FILE *outstream)
 "  Options:\n"
 "    -d|--debug             print detailed debugging messages to terminal\n"
 "                           (standard error)\n"
-"    -e|--emptyseqs         when reporting interval loci, include sequences\n"
-"                           with no annotated genes\n"
 "    -g|--genemap: FILE     print a mapping from each gene annotation to its\n"
 "                           corresponding locus to the given file\n"
 "    -h|--help              print this help message and exit\n"
@@ -134,7 +127,7 @@ void print_usage(FILE *outstream)
 int main(int argc, char **argv)
 {
   // Parse options from command line
-  LocusPocusOptions options = { 0, NULL, 0, 500, stdout, 0, 0, NULL, 0 };
+  LocusPocusOptions options = { 0, NULL, 0, 500, stdout, 0, NULL, 0 };
   parse_options(argc, argv, &options);
   int numfiles = argc - optind;
   if(numfiles < 1)
@@ -177,8 +170,7 @@ int main(int argc, char **argv)
     if(options.intloci)
     {
        seqloci = agn_locus_index_interval_loci(loci, seqid, options.delta,
-                                               options.skipends,
-                                               options.reportemptyseqs);
+                                               options.skipends);
     }
     else
     {
