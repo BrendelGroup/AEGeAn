1b69a37b0fa304f50d0e185bc70e35fea2f41cf6
Daniel Standage <daniel.standage@gmail.com>
2014-03-28 09:50:06 -0400

    Changes to facilitate summary-only mode

--- src/core/AgnCompareTextReportVisitor.c
+++ src/core/AgnCompareTextReportVisitor.c
@@ -1,4 +1,5 @@
 #include <math.h>
+#include <stdarg.h>
 #include "AgnCliquePair.h"
 #include "AgnCompareTextReportVisitor.h"
 #include "AgnLocus.h"
@@ -75,6 +76,11 @@ static void locus_report_print_unmatched_cliques(AgnCompareTextReportVisitor *v,
  */
 static void print_locus_report(AgnCompareTextReportVisitor *v, AgnLocus *locus);
 
+/**
+ * @function FIXME
+ */
+static void printf_c(FILE *outstream, const char *format, ...);
+
 
 //------------------------------------------------------------------------------
 // Method implementations
@@ -149,38 +155,39 @@ static void locus_print_nucleotide_report(FILE *outstream,
                     (double)pairstats->overall_length;
   double tolerance = agn_clique_pair_tolerance(pair);
   if(fabs(identity - 1.0) < tolerance)
-      fprintf(outstream, "     |    Gene structures match perfectly!\n");
+    printf_c(outstream, "     |    Gene structures match perfectly!\n");
   else
   {
-    fprintf(outstream, "     |    %-30s %-10s %-10s %-10s\n",
-            "Nucleotide-level comparison", "CDS", "UTRs", "Overall" );
-    fprintf(outstream, "     |    %-30s %-10s %-10s %.3lf\n",
-            "Matching coefficient:", pairstats->cds_nuc_stats.mcs,
-            pairstats->utr_nuc_stats.mcs, identity);
-    fprintf(outstream, "     |    %-30s %-10s %-10s %-10s\n",
-            "Correlation coefficient:", pairstats->cds_nuc_stats.ccs,
-            pairstats->utr_nuc_stats.ccs, "--");
-    fprintf(outstream, "     |    %-30s %-10s %-10s %-10s\n", "Sensitivity:",
-            pairstats->cds_nuc_stats.sns, pairstats->utr_nuc_stats.sns, "--");
-    fprintf(outstream, "     |    %-30s %-10s %-10s %-10s\n", "Specificity:",
-            pairstats->cds_nuc_stats.sps, pairstats->utr_nuc_stats.sps, "--");
-    fprintf(outstream, "     |    %-30s %-10s %-10s %-10s\n", "F1 Score:",
-            pairstats->cds_nuc_stats.f1s, pairstats->utr_nuc_stats.f1s, "--");
-    fprintf(outstream, "     |    %-30s %-10s %-10s %-10s\n", "Annotation edit distance:",
-            pairstats->cds_nuc_stats.eds, pairstats->utr_nuc_stats.eds, "--");
+    printf_c(outstream, "     |    %-30s %-10s %-10s %-10s\n",
+             "Nucleotide-level comparison", "CDS", "UTRs", "Overall" );
+    printf_c(outstream, "     |    %-30s %-10s %-10s %.3lf\n",
+             "Matching coefficient:", pairstats->cds_nuc_stats.mcs,
+             pairstats->utr_nuc_stats.mcs, identity);
+    printf_c(outstream, "     |    %-30s %-10s %-10s %-10s\n",
+             "Correlation coefficient:", pairstats->cds_nuc_stats.ccs,
+             pairstats->utr_nuc_stats.ccs, "--");
+    printf_c(outstream, "     |    %-30s %-10s %-10s %-10s\n", "Sensitivity:",
+             pairstats->cds_nuc_stats.sns, pairstats->utr_nuc_stats.sns, "--");
+    printf_c(outstream, "     |    %-30s %-10s %-10s %-10s\n", "Specificity:",
+             pairstats->cds_nuc_stats.sps, pairstats->utr_nuc_stats.sps, "--");
+    printf_c(outstream, "     |    %-30s %-10s %-10s %-10s\n", "F1 Score:",
+             pairstats->cds_nuc_stats.f1s, pairstats->utr_nuc_stats.f1s, "--");
+    printf_c(outstream, "     |    %-30s %-10s %-10s %-10s\n",
+             "Annotation edit distance:", pairstats->cds_nuc_stats.eds,
+             pairstats->utr_nuc_stats.eds, "--");
   }
 
-  fprintf(outstream, "     |\n");
+  printf_c(outstream, "     |\n");
 }
 
 static void locus_print_structure_report(FILE *outstream,
                                          AgnCompStatsBinary *stats,
                                          const char *label, const char *units)
 {
-  fprintf(outstream, " | %s structure comparison\n", label);
+  printf_c(outstream, " | %s structure comparison\n", label);
   if(stats->missing == 0 && stats->wrong == 0)
   {
-    fprintf(outstream,
+    printf_c(outstream,
             "     |    %lu reference  %s\n"
             "     |    %lu prediction %s\n"
             "     |    %s structures match perfectly!\n",
@@ -188,7 +195,7 @@ static void locus_print_structure_report(FILE *outstream,
   }
   else
   {
-    fprintf(outstream,
+    printf_c(outstream,
             "     |    %lu reference %s\n"
             "     |        %lu match prediction\n"
             "     |        %lu don't match prediction\n"
@@ -199,7 +206,7 @@ static void locus_print_structure_report(FILE *outstream,
              stats->correct, stats->missing,
              stats->correct + stats->wrong, units,
              stats->correct, stats->wrong);
-    fprintf(outstream,
+    printf_c(outstream,
             "     |    %-30s %-10s\n" 
             "     |    %-30s %-10s\n" 
             "     |    %-30s %-10s\n" 
@@ -208,7 +215,7 @@ static void locus_print_structure_report(FILE *outstream,
             "Annotation edit distance:", stats->sns, stats->sps, stats->f1s,
             stats->eds);
   }
-  fprintf(outstream, "     |\n");
+  printf_c(outstream, "     |\n");
 }
 
 static void locus_report_print_geneids(AgnCompareTextReportVisitor *v,
@@ -217,27 +224,27 @@ static void locus_report_print_geneids(AgnCompareTextReportVisitor *v,
   GtArray *genes;
 
   genes = agn_locus_refr_gene_ids(locus);
-  fprintf(v->fp_reports, "  |  reference genes:\n");
+  printf_c(v->fp_reports, "  |  reference genes:\n");
   if(gt_array_size(genes) == 0)
-    fprintf(v->fp_reports, "|    None!\n");
+    printf_c(v->fp_reports, "|    None!\n");
   while(gt_array_size(genes) > 0)
   {
     const char **geneid = gt_array_pop(genes);
-    fprintf(v->fp_reports, "|    %s\n", *geneid);
+    printf_c(v->fp_reports, "|    %s\n", *geneid);
   }
-  fprintf(v->fp_reports, "|\n");
+  printf_c(v->fp_reports, "|\n");
   gt_array_delete(genes);
 
   genes = agn_locus_pred_gene_ids(locus);
-  fprintf(v->fp_reports, "  |  prediction genes:\n");
+  printf_c(v->fp_reports, "  |  prediction genes:\n");
   if(gt_array_size(genes) == 0)
-    fprintf(v->fp_reports, "|    None!\n");
+    printf_c(v->fp_reports, "|    None!\n");
   while(gt_array_size(genes) > 0)
   {
     const char **geneid = gt_array_pop(genes);
-    fprintf(v->fp_reports, "|    %s\n", *geneid);
+    printf_c(v->fp_reports, "|    %s\n", *geneid);
   }
-  fprintf(v->fp_reports, "|\n");
+  printf_c(v->fp_reports, "|\n");
   gt_array_delete(genes);
 }
 
@@ -249,33 +256,33 @@ static void locus_report_print_pair(AgnCompareTextReportVisitor *v,
 
   refrclique = agn_clique_pair_get_refr_clique(pair);
   tids = agn_transcript_clique_ids(refrclique);
-  fprintf(v->fp_reports, "     |  reference transcripts:\n");
+  printf_c(v->fp_reports, "     |  reference transcripts:\n");
   while(gt_array_size(tids) > 0)
   {
     const char **tid = gt_array_pop(tids);
-    fprintf(v->fp_reports, "     |    %s\n", *tid);
+    printf_c(v->fp_reports, "     |    %s\n", *tid);
   }
   gt_array_delete(tids);
 
   predclique = agn_clique_pair_get_pred_clique(pair);
   tids = agn_transcript_clique_ids(predclique);
-  fprintf(v->fp_reports, "     |  prediction transcripts:\n");
+  printf_c(v->fp_reports, "     |  prediction transcripts:\n");
   while(gt_array_size(tids) > 0)
   {
     const char **tid = gt_array_pop(tids);
-    fprintf(v->fp_reports, "     |    %s\n", *tid);
+    printf_c(v->fp_reports, "     |    %s\n", *tid);
   }
   gt_array_delete(tids);
 
-  fprintf(v->fp_reports, "     |\n");
+  printf_c(v->fp_reports, "     |\n");
 
   if(v->gff3)
   {
-    fprintf(v->fp_reports, " | reference GFF3:\n");
+    printf_c(v->fp_reports, " | reference GFF3:\n");
     agn_transcript_clique_to_gff3(refrclique, v->fp_reports, " | ");
-    fprintf(v->fp_reports, " | prediction GFF3:\n");
+    printf_c(v->fp_reports, " | prediction GFF3:\n");
     agn_transcript_clique_to_gff3(predclique, v->fp_reports, " | ");
-    fprintf(v->fp_reports, " |\n");
+    printf_c(v->fp_reports, " |\n");
   }
 
   AgnComparison *pairstats = agn_clique_pair_get_stats(pair);
@@ -296,30 +303,30 @@ static void locus_report_print_unmatched_cliques(AgnCompareTextReportVisitor *v,
   GtArray *unique_refr_cliques = agn_locus_get_unique_refr_cliques(locus);
   if(unique_refr_cliques && gt_array_size(unique_refr_cliques) > 0)
   {
-    fprintf(v->fp_reports, "     |\n");
-    fprintf(v->fp_reports, "     |  reference transcripts (or transcript sets) "
-            "without a prediction match\n");
+    printf_c(v->fp_reports, "     |\n");
+    printf_c(v->fp_reports, "     |  reference transcripts (or transcript "
+             "sets) without a prediction match\n");
   }
   for(i = 0; i < gt_array_size(unique_refr_cliques); i++)
   {
     AgnTranscriptClique **clique = gt_array_get(unique_refr_cliques, i);
     char *tid = agn_transcript_clique_id(*clique);
-    fprintf(v->fp_reports, "     | [%s]\n", tid);
+    printf_c(v->fp_reports, "     | [%s]\n", tid);
     gt_free(tid);
   }
 
   GtArray *unique_pred_cliques = agn_locus_get_unique_pred_cliques(locus);
   if(unique_pred_cliques && gt_array_size(unique_pred_cliques) > 0)
   {
-    fprintf(v->fp_reports, "     |\n");
-    fprintf(v->fp_reports, "     |  novel prediction transcripts (or transcript"
-            " sets)\n");
+    printf_c(v->fp_reports, "     |\n");
+    printf_c(v->fp_reports, "     |  novel prediction transcripts (or "
+             "transcript sets)\n");
   }
   for(i = 0; i < gt_array_size(unique_pred_cliques); i++)
   {
     AgnTranscriptClique **clique = gt_array_get(unique_pred_cliques, i);
     char *tid = agn_transcript_clique_id(*clique);
-    fprintf(v->fp_reports, "     | [%s]\n", tid);
+    printf_c(v->fp_reports, "     | [%s]\n", tid);
     gt_free(tid);
   }
 }
@@ -328,7 +335,7 @@ static void print_locus_report(AgnCompareTextReportVisitor *v, AgnLocus *locus)
 {
   GtRange range = gt_genome_node_get_range(locus);
   GtStr *seqid = gt_genome_node_get_seqid(locus);
-  fprintf(v->fp_reports,
+  printf_c(v->fp_reports,
           "|-------------------------------------------------\n"
           "|---- Locus: %s_%lu-%lu\n"
           "|-------------------------------------------------\n"
@@ -336,14 +343,14 @@ static void print_locus_report(AgnCompareTextReportVisitor *v, AgnLocus *locus)
           gt_str_get(seqid), range.start, range.end);
 
   locus_report_print_geneids(v, locus);
-  fprintf(v->fp_reports,
+  printf_c(v->fp_reports,
           "|\n"
           "|----------\n");
 
   GtArray *pairs2report = agn_locus_pairs_to_report(locus);
   if(pairs2report == NULL || gt_array_size(pairs2report) == 0)
   {
-    fprintf(v->fp_reports,
+    printf_c(v->fp_reports,
             "     |\n"
             "     | No comparisons were performed for this locus.\n"
             "     |\n");
@@ -354,21 +361,32 @@ static void print_locus_report(AgnCompareTextReportVisitor *v, AgnLocus *locus)
   for(i = 0; i < gt_array_size(pairs2report); i++)
   {
     AgnCliquePair *pair = *(AgnCliquePair **)gt_array_get(pairs2report, i);
-    fprintf(v->fp_reports,
+    printf_c(v->fp_reports,
             "     |\n"
             "     |--------------------------\n"
             "     |---- Begin comparison ----\n"
             "     |--------------------------\n"
             "     |\n");
     locus_report_print_pair(v, pair);
-    fprintf(v->fp_reports,
+    printf_c(v->fp_reports,
             "     |\n"
             "     |--------------------------\n"
-            "     |----= End comparison -----\n"
+            "     |----- End comparison -----\n"
             "     |--------------------------\n"
             "     |\n");
   }
 
   locus_report_print_unmatched_cliques(v, locus);
-  fprintf(v->fp_reports, "\n");
+  printf_c(v->fp_reports, "\n");
+}
+
+static void printf_c(FILE *outstream, const char *format, ...)
+{
+  va_list ap;
+  if(outstream == NULL)
+    return;
+
+  va_start(ap, format);
+  vfprintf(outstream, format, ap);
+  va_end(ap);
 }
