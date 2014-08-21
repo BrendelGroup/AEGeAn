cadc2aa826d5a2dc043d882815a7b018842ab908
Daniel Standage <daniel.standage@gmail.com>
2014-08-21 09:59:52 -0400

    Cleaned up pmrna

--- src/pmrna.c
+++ src/pmrna.c
@@ -3,34 +3,117 @@
 #include "AgnMrnaRepVisitor.h"
 #include "AgnPseudogeneFixVisitor.h"
 
-int main()
+typedef struct
 {
-  gt_lib_init();
+  bool infer_introns;
+  FILE *outstream;
+  bool fix_pseudogenes;
+} PmrnaOptions;
 
-  GtNodeStream *in = gt_gff3_in_stream_new_unsorted(0, NULL);
-  gt_gff3_in_stream_check_id_attributes((GtGFF3InStream *)in);
-  gt_gff3_in_stream_enable_tidy_mode((GtGFF3InStream *)in);
-  GtNodeStream *pfs = agn_pseudogene_fix_stream_new(in);
-  GtNodeStream *ais = gt_add_introns_stream_new(pfs);
-  GtNodeStream *rep_stream = agn_mrna_rep_stream_new(ais);
-  GtFile *fh = gt_file_new_from_fileptr(stdout);
-  GtNodeStream *out = gt_gff3_out_stream_new(rep_stream, fh);
-  gt_gff3_out_stream_retain_id_attributes((GtGFF3OutStream *)out);
+static void print_usage(FILE *outstream)
+{
+  fprintf(outstream,
+"\npmrna: filter out all but the primary isoform from each gene of the input\n"
+"Usage: pmrna [options] < annot.gff3 > new.gff3\n"
+"  Options:\n"
+"    -h|--help           print this help message and exit\n"
+"    -i|--introns        flag indicating that introns are declared explicitly\n"
+"                        and do not need to be inferred from exon features;\n"
+"                        default is to infer introns\n"
+"    -p|--pseudogenes    disable pseudogene detection and correction\n\n");
+}
 
-  GtError *error = gt_error_new();
-  int result = gt_node_stream_pull(out, error);
-  if(result == -1)
+static void parse_options(int argc, char **argv, PmrnaOptions *options)
+{
+  int opt = 0;
+  int optindex = 0;
+  const char *optstr = "hip";
+  const struct option pmrna_options[] =
   {
-    fprintf(stderr, "Error processing node stream: %s\n", gt_error_get(error));
+    { "help",        no_argument,       NULL, 'h' },
+    { "introns",     no_argument,       NULL, 'i' },
+    { "pseudogenes", no_argument,       NULL, 'o' },
+  };
+  for(opt  = getopt_long(argc, argv + 0, optstr, pmrna_options, &optindex);
+      opt != -1;
+      opt  = getopt_long(argc, argv + 0, optstr, pmrna_options, &optindex))
+  {
+    switch(opt)
+    {
+      case 'h':
+        print_usage(stdout);
+        exit(0);
+        break;
+      case 'i':
+        options->infer_introns = false;
+        break;
+      case 'p':
+        options->fix_pseudogenes = false;
+        break;
+    }
   }
+}
+
+int main(int argc, char **argv)
+{
+  GtError *error;
+  GtNodeStream *stream, *last_stream;
+  GtQueue *streams;
+  PmrnaOptions options = { true, NULL, true };
+  parse_options(argc, argv, &options);
+
+  //----------
+  // Set up the processing stream
+  //----------
+  gt_lib_init();
+  streams = gt_queue_new();
+
+  stream = gt_gff3_in_stream_new_unsorted(0, NULL);
+  gt_gff3_in_stream_check_id_attributes((GtGFF3InStream *)stream);
+  gt_gff3_in_stream_enable_tidy_mode((GtGFF3InStream *)stream);
+  gt_queue_add(streams, stream);
+  last_stream = stream;
+
+  if(options.fix_pseudogenes)
+  {
+    stream = agn_pseudogene_fix_stream_new(last_stream);
+    gt_queue_add(streams, stream);
+    last_stream = stream;
+  }
+
+  if(options.infer_introns)
+  {
+    stream = gt_add_introns_stream_new(last_stream);
+    gt_queue_add(streams, stream);
+    last_stream = stream;
+  }
+
+  stream = agn_mrna_rep_stream_new(last_stream);
+  gt_queue_add(streams, stream);
+  last_stream = stream;
+
+  stream = gt_gff3_out_stream_new(last_stream, NULL);
+  gt_gff3_out_stream_retain_id_attributes((GtGFF3OutStream *)stream);
+  gt_queue_add(streams, stream);
+  last_stream = stream;
+
+  //----------
+  // Execute the processing stream
+  //----------
+  error = gt_error_new();
+  if(gt_node_stream_pull(last_stream, error) == -1)
+    fprintf(stderr, "Error processing node stream: %s\n", gt_error_get(error));
   gt_error_delete(error);
 
-  gt_node_stream_delete(out);
-  gt_node_stream_delete(rep_stream);
-  gt_node_stream_delete(ais);
-  gt_node_stream_delete(pfs);
-  gt_node_stream_delete(in);
-  gt_file_delete_without_handle(fh);
+  //----------
+  // Free memory
+  //----------
+  while(gt_queue_size(streams) > 1)
+  {
+    stream = gt_queue_get(streams);
+    gt_node_stream_delete(stream);
+  }
+  gt_queue_delete(streams);
   gt_lib_clean();
   return 0;
 }
