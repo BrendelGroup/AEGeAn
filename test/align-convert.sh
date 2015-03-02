a7d232c76d8b5b92108adf1460bb54d56857cae8
Daniel Standage <daniel.standage@gmail.com>
2015-03-02 13:40:39 -0500

    Another test case in support of #128.

--- test/align-convert.sh
+++ test/align-convert.sh
@@ -4,15 +4,18 @@ set -eo pipefail
 echo "    AEGeAn::align-convert.py"
 tempfile="align-convert.temp"
 
-data/scripts/align-convert.py \
-    < data/gff3/gaeval-example1-in.gff3 \
-    > $tempfile
+for label in example1 example5
+do
+  data/scripts/align-convert.py \
+      < data/gff3/gaeval-${label}-in.gff3 \
+      > $tempfile
 
-diff $tempfile data/gff3/gaeval-example1-fixed.gff3 > /dev/null
-status=$?
-result="FAIL"
-if [[ $status == 0 ]]; then
-  result="PASS"
-fi
-printf "        | %-36s | %s\n" "GAEVAL example 1" $result
-rm $tempfile
+  diff $tempfile data/gff3/gaeval-${label}-fixed.gff3 > /dev/null
+  status=$?
+  result="FAIL"
+  if [[ $status == 0 ]]; then
+    result="PASS"
+  fi
+  printf "        | %-36s | %s\n" "GAEVAL $label" $result
+  rm $tempfile
+done
