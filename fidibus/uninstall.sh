94d4a5208acd6804bf4fc7a90a81b77e3697c88b
vpbrendel <vbrendel@indiana.edu>
2020-01-04 16:21:27 -0500

    force python3 installation of fidibus

--- fidibus/uninstall.sh
+++ fidibus/uninstall.sh
@@ -2,7 +2,7 @@
 #
 # ... use ".uninstall.sh --user" to uninstall a user local copy of fidibus.
 
-python setup.py install $1 --record files.txt
+python3 setup.py install $1 --record files.txt
 
 cat files.txt | xargs rm -rf
 \rm -rf fidibus.egg-info files.txt fidibus.egg-info dist build versioneer.pyc
